aadccf6253bf0c591d1d4bbd31f960ed18095e5b
Alex Belov <62434456+Vaelastras@users.noreply.github.com>
2021-09-03 15:21:43 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-//const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3000'}`;
+//  const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3000'}`;
 const BASE_URL = 'https://news-explorer-project.herokuapp.com';
 const API_KEY = '8c43c8fd4f0c449e95267fecb55fb71b';
 const PROXY_URL = 'https://nomoreparties.co/news/v2/everything';
